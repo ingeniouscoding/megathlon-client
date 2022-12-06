9f4b943daedfd31f718588660465e6c92d6c5134
dimchik_st@list.ru <dimchik_st@list.ru>
2022-12-06 19:29:52 +0300

    Install ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
+import { reducers } from './app.state.';
 import { AppComponent } from './global/core/components';
 import { CoreModule } from './global/core/core.module';
 import { LayoutModule } from './global/layout/layout.module';
+import { CustomRouteSerializer } from './global/router/custom-route-serializer';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {}),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({
+      serializer: CustomRouteSerializer,
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     LayoutModule,
     CoreModule,
-    BrowserAnimationsModule,
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
